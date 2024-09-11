54c75c2fb757c2d43f5fe45e93f4f238c86ae0c4
SeregaUrban <7236187@mail.ru>
2024-09-11 15:31:02 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+may_string = input('Введите название своего университета: ')
+print ('В названии вашего университета:', may_string.__len__(),"букв")
+print(may_string.upper())
+print(may_string.lower())
+print(may_string.replace(' ',''))
+print(may_string[0])
+print(may_string[-1])
+
